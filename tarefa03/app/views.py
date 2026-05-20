e75ccc90a71d2134caa124c22ac500373fa97c2b
José Cassiano <jcnetox08@gmail.com>
2026-05-19 23:24:54 -0300

    tarefa03 concluída

--- tarefa03/app/views.py
+++ tarefa03/app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, "index.html")
+
+def usuarios(request):
+    lista_users = [
+        {"nome": "Kaio Cruz", "matricula": 111, "idade": 17, "cidade": "São Paulo do Potengi"},
+        {"nome": "Big Jhow", "matricula": 222, "idade": 15, "cidade": "Lagoa de Velhos"},
+        {"nome": "Xexeu", "matricula": 333, "idade": 17, "cidade": "Bom Jesus"},
+        {"nome": "Cassioto", "matricula": 444, "idade": 17, "cidade": "São Tomé"},
+        {"nome": "Juninho", "matricula": 555, "idade": 18, "cidade": "São Tomé"},
+    ]
+
+    context = {
+        "usuarios": lista_users,
+    }
+    
+    return render(request, "usuarios.html", context)
