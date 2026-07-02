086477156cf26f26f18e97e08c43a7c270e4e7a4
José Cassiano <jcnetox08@gmail.com>
2026-07-01 23:49:47 -0300

    registro da classe Tarefa + adição do __str__

--- tarefa04/app/models.py
+++ tarefa04/app/models.py
@@ -4,3 +4,6 @@ class Tarefa(models.Model):
     nome = models.CharField(max_length=100)
     status = models.BooleanField(default=False)
     prazo = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.nome
